79c10e807016954b7b1b974280ce046d296b6f54
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-06-28 23:27:05 +0500

    Index05

--- Index05.py
+++ Index05.py
@@ -1,9 +1,40 @@
+from gettext import find
+
+
 def main(s):
     """
-    Given a variable s string of length five. Determine the number of digits involved in this variable.
+    Given a variable s string of length five. Determine the number of digits 
+    involved in this variable.
     Args:
         s(str): parameter
     Returns:
         int: answer
     """
-    return
+    n = 0
+    if s[0].isdigit():
+        n+=1
+    else:
+        0
+
+    if s[1].isdigit():
+        n+=1
+    else:
+        0
+
+    if s[2].isdigit():
+        n+=1
+    else:
+        0
+    
+    if s[3].isdigit():
+        n+=1
+    else:
+        0
+    
+    if s[4].isdigit():
+        n+=1
+    else:
+        0
+
+    return n
+print(main("32g4h8"))
