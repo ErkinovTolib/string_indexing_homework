047202549c2fa23f3384dd56aa3b4961bc616193
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-06-28 23:11:21 +0500

    Index06

--- Index06.py
+++ Index06.py
@@ -1,9 +1,11 @@
 def main(s):
     """
-    A string variable consisting of several characters is given. Add and return the first and last character.
+    A string variable consisting of several characters is given. 
+    Add and return the first and last character.
     Args:
         s(str): parameter
     Returns:
         str: answer
     """
-    return
+    return s[0]+s[-1]
+print(main("good"))
